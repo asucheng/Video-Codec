a20c80271d326796a609d4a449033c8140c60b21
yangjp22 <jinyang0604@gmail.com>
2024-11-27 18:13:15 -0500

    1. check A1_Q3_calcResidual.m file

--- A1_Q3_calcResidual.m
+++ A1_Q3_calcResidual.m
@@ -5,8 +5,4 @@ function approxResidualBlock = A1_Q3_calcResidual(predictedBlock, currentBlock, 
     % Approximated residuals for n = 1, 2, 3
     approxResidualBlock = round(residualBlock / (2^n)) * (2^n);
     
-    % Save the approximated residual
-    % fprintf(residualFile, 'Frame %d, Block (%d, %d), n=%d:\n', frameIdx, row, col, n);
-    % fprintf(residualFile, '%d ', approxResidualBlock(:));
-    % fprintf(residualFile, '\n');
 end
